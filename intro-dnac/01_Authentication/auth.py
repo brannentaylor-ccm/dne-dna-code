1676eea39d42b3c51e0d618254e0615ec9a8dd84
brannentaylor-ccm <brannen.taylor@ccm.com>
2025-02-03 19:56:10 -0500

    changed auth to use sessions

--- intro-dnac/01_Authentication/auth.py
+++ intro-dnac/01_Authentication/auth.py
@@ -2,6 +2,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import os
 import sys
+import json
 
 # Get the absolute path for the directory where this file is located "here"
 here = os.path.abspath(os.path.dirname(__file__))
@@ -18,17 +19,47 @@ DNAC_URL = env_lab.DNA_CENTER["host"]
 DNAC_USER = env_lab.DNA_CENTER["username"]
 DNAC_PASS = env_lab.DNA_CENTER["password"]
 
-def get_auth_token():
+def get_auth_session(scheme:str='https://', url:str='', api_endpoint:str=''):
     """
     Building out Auth request. Using requests.post to make a call to the Auth Endpoint
+
+    Returns:
+    session.headers:requests.structures.CaseInsensitiveDict     - returning a dictionary LIKE object
+    headers:dict                                                - changed the session.headers to a dictionary
+
     """
-    url = 'https://{}/dna/system/api/v1/auth/token'.format(DNAC_URL)                      # Endpoint URL
-    hdr = {'content-type' : 'application/json'}                                           # Define request header
-    resp = requests.post(url, auth=HTTPBasicAuth(DNAC_USER, DNAC_PASS), headers=hdr)      # Make the POST Request
-    token = resp.json()['Token']                                                          # Retrieve the Token
-    print("Token Retrieved: {}".format(token))                                            # Print out the Token
-    return token    # Create a return statement to send the token back for later use
+    if not api_endpoint:
+        api_endpoint = "/dna/system/api/v1/auth/token"
+
+    if not url:
+        url = f'{scheme}{DNAC_URL}{api_endpoint}'
+
+    hdr = {'content-type' : 'application/json'}                                                         # Define request header
+    
+    # Instead of using requests, we're going to use session for session management.  Create a session instance.
+    session = requests.Session()
+    # use the Session.post method, to make the call to get the token
+    s_response = session.post(url, auth=HTTPBasicAuth(DNAC_USER, DNAC_PASS), headers=hdr, verify=False)      # Make the POST Request, do not verify certificate
+    # The token is in the resonse to the session post.  Update the session headers, to change the authorization to a bearer token.
+    session.headers.update({'Authorization': f"Bearer {s_response.json().get('Token')}"})
+    # Session headers is a requests.structures.CaseInsensitiveDict .  Change it to a dictionary.
+    headers = dict(session.headers)
+    # print the dumps, nicely.
+                   
+    return session.headers, headers    #Return 
 
 
 if __name__ == "__main__":
-    get_auth_token()
+    requests.packages.urllib3.disable_warnings()
+    # include the token in Authorization header for subsequent requests
+
+    session_headers, headers = get_auth_session()
+    msg = "\nGot the token, modified the session headers to:"
+    print(f"{msg}\n{'-'*len(msg)}")
+    print(json.dumps(headers, indent=2))
+
+    #ima comment
+    #another
+    #another
+
+    # token = resp.json().get("Token")
